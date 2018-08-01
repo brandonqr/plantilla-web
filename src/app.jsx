01c48b7b189e0482c508313ee47080f274df3e2a
develooper24 <brandonqr1@gmail.com>
2018-08-01 22:23:03 +0200

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+// Import react
+import React from 'react';
+import Navbar from './componentes/Nabvar/Navbar.jsx';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Import resources
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+
+// Import bootstrap css
+import "bootstrap/dist/css/bootstrap.min.css";
+//Import Bootstrap js
+import "bootstrap/dist/js/bootstrap.bundle";
+
+// Render main app
+ReactDOM.render( < App / > , document.getElementById("root"));
